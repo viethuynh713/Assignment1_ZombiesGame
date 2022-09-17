58cbd255f5f3e7e58799fd27c40682c4f03d38ca
Khánh <77840594+sshinichi-69@users.noreply.github.com>
2022-09-17 12:08:25 +0700

    add emeny logic

---
+++ src/Enemy.py
@@ -0,0 +1,23 @@
+import imp
+
+
+import pygame
+from pygame.locals import *
+import EnemyManager
+import Player
+
+class Enemy:
+    def __init__(self, enemyManager: EnemyManager, position: tuple) -> None:
+        self.enemyManager = enemyManager
+        widthHitBox = 30
+        heightHitBox = 50
+        self.hitBox = Rect(position[0] - widthHitBox / 2, position[1] - heightHitBox, widthHitBox, heightHitBox)
+    def hitHammer(self, player: Player) -> None:
+        player.incHitScore()
+        self.enemyManager.removeEnemy(self)
+    def isCollideHammer(self, position: tuple) -> bool:
+        if self.hitBox.collidepoint(position[0], position[1]):
+            return True
+        return False
+    def getHitBox(self) -> Rect:
+        return self.hitBox
--- src/Player.py
+++ src/Player.py
@@ -1,3 +1,5 @@
 class Player:
     def __init__(self) -> None:
         pass
+    def incHitScore(self) -> None:
+        pass
