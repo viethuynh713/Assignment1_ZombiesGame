8b63f099c757082077857a922ad6a186361ee688
giabao04082001 <giabao04082001@gmail.com>
2022-09-17 11:15:41 +0700

    ! init background

--- src/Player.py
+++ src/Player.py
@@ -1,3 +1,15 @@
+import main
+import pygame
+from pygame.locals import *
+
 class Player:
-    def __init__(self) -> None:
+    def __init__(self, score, combo, comboTime):
+        self.score = score
+        self.combo = combo
+        self.comboTime = comboTime
+    def knockEnemy(self):
+        pass
+    def addScore(self):
+        pass
+    def subtractScore(self):
         pass
